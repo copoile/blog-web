51af45a01b038fa3a6ad158a980ee9182bd40103
yahow <726856005@qq.com>
2020-03-24 22:03:33 +0800

    用户管理静态页面

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -4,21 +4,30 @@
       <h1>个人信息</h1>
       <ul class="setting-list">
         <li class="item">
-          头像
+          <span class="label">头像</span>
+          <div class="avatar-wrapper">
+            <img :src="userInfo.avatar || defaultAvatar" class="user-avatar">
+          </div>
+          <div class="action-box">
+            <div class="hint">支持 jpg、png 格式大小 200KB 以内的图片</div>
+            <el-button type="primary" size="mini" style="padding: 5px 7px;">点击上传</el-button>
+          </div>
         </li>
         <li class="item">
           <span class="label">用户名</span>
           <div class="input-wrapper">
-            <el-input v-model="title" placeholder="填写你的用户名" />
+            <el-input v-model="userInfo.username" disabled placeholder="填写你的用户名" />
           </div>
           <div class="action-box">
-            <span><i class="el-icon-edit" />修改</span>
+            <el-button type="text" disabled style="color: #C0C4CC;">
+              <span><i class="el-icon-edit" />修改</span>
+            </el-button>
           </div>
         </li>
         <li class="item">
           <span class="label">昵称</span>
           <div class="input-wrapper">
-            <el-input v-model="title" placeholder="填写你的昵称" />
+            <el-input v-model="form.nickname" placeholder="填写你的昵称" />
           </div>
           <div class="action-box">
             <span><i class="el-icon-edit" />修改</span>
@@ -27,47 +36,64 @@
         <li class="item">
           <span class="label">手机号</span>
           <div class="input-wrapper">
-            <el-input v-model="title" placeholder="绑定你的手机号" />
+            <el-input v-model="form.mobile" disabled placeholder="绑定你的手机号" />
           </div>
           <div class="action-box">
-            <span><i class="el-icon-edit" />更改绑定</span>
+            <el-button type="text">
+              <span><i class="el-icon-mobile-phone" />更改绑定</span>
+            </el-button>
           </div>
         </li>
         <li class="item">
           <span class="label">邮箱</span>
           <div class="input-wrapper">
-            <el-input v-model="title" placeholder="未绑定邮箱" />
+            <el-input v-model="form.email" disabled placeholder="未绑定邮箱" />
           </div>
           <div class="action-box">
-            <span><i class="el-icon-edit" />立即绑定</span>
+            <el-button type="text">
+              <span><i class="el-icon-message" />立即绑定</span>
+            </el-button>
           </div>
         </li>
         <li class="item">
           <span class="label">生日</span>
           <div class="input-wrapper">
-            <el-input v-model="title" placeholder="选择你的生日" />
+            <el-date-picker v-model="form.birthday" type="date" placeholder="选择日期" />
           </div>
           <div class="action-box">
-            <span><i class="el-icon-edit" />修改</span>
+            <el-button type="text">
+              <span><i class="el-icon-date" />修改</span>
+            </el-button>
           </div>
         </li>
         <li class="item">
           <span class="label">性别</span>
           <div class="input-wrapper">
-            <el-input v-model="title" placeholder="选择你的性别" />
+            <el-select v-model="form.gender" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="form.gender"
+              />
+            </el-select>
           </div>
           <div class="action-box">
-            <span><i class="el-icon-edit" />修改</span>
+            <el-button type="text">
+              <span><i class="el-icon-female" />修改</span>
+            </el-button>
           </div>
         </li>
 
         <li class="item">
           <span class="label">简介</span>
           <div class="input-wrapper">
-            <el-input v-model="title" placeholder="填写你的简介" />
+            <el-input v-model="form.brief" type="textarea" :rows="2" placeholder="填写你的简介" />
           </div>
           <div class="action-box">
-            <span><i class="el-icon-edit" />修改</span>
+            <el-button type="text">
+              <span><i class="el-icon-edit" />修改</span>
+            </el-button>
           </div>
         </li>
       </ul>
@@ -77,19 +103,48 @@
 
 <script>
 import { mapGetters } from 'vuex'
-
 export default {
-  name: 'Dashboard',
+  name: 'User',
   data() {
     return {
-      title: ''
+      options: [
+        {
+          value: 1,
+          label: '男'
+        },
+        {
+          value: 0,
+          label: '女'
+        }
+      ],
+      userInfo: '',
+      form: {
+        nickname: '',
+        mobile: '',
+        email: '',
+        gender: 0,
+        birthday: '',
+        brief: ''
+      }
     }
   },
   computed: {
     ...mapGetters([
-      'userInfo',
-      'roles'
+      'defaultAvatar'
     ])
+  },
+  mounted() {
+    const userInfo = this.$store.getters.userInfo
+    this.userInfo = userInfo
+    this.username = userInfo.username
+    this.form.nickname = userInfo.nickname
+    this.form.mobile = userInfo.mobile
+    this.form.email = userInfo.email
+    this.form.gender = userInfo.gender
+    this.originalGender = userInfo.gender
+    this.form.birthday = userInfo.birthday
+    this.originalBirthday = userInfo.birthday
+    this.form.brief = userInfo.brief
   }
 }
 </script>
@@ -103,20 +158,21 @@ export default {
   position: relative;
 
   .setting-box {
-    margin-top: 5vh;
-    margin-left: 30vw;
+    margin-top: 2vh;
+    margin-left: 50%;
+    transform: translateX(-50%);
     width: 600px;
     max-width: 100%;
     background: #fff;
 
-    padding: 20px 20px;
+    padding: 20px;
+    padding-top: 10px;
 
     h1 {
       font-size: 20px;
       font-weight: 700;
       padding: 0;
-      padding-bottom: 15px;
-      border-bottom: 1px solid #f1f1f1;
+      padding-bottom: 10px;
     }
 
     .setting-list {
@@ -125,8 +181,8 @@ export default {
 
       .item {
         list-style: none;
-        border-bottom: 1px solid #f1f1f1;
-        padding: 24px 0;
+        border-top: 1px solid #f1f1f1;
+        padding: 20px 0;
         line-height: 24px;
         display: flex;
         align-items: center;
@@ -141,6 +197,13 @@ export default {
             border: none;
             border-bottom: none;
             border-radius: 0;
+            background: #fff!important;
+          }
+
+          & /deep/ .el-textarea__inner {
+            border: none;
+            border-bottom: none;
+            border-radius: 0;
           }
         }
 
@@ -148,6 +211,31 @@ export default {
           font-size: 12px;
           color: #007fff;
           margin-right: 10px;
+          margin-left: 10px;
+
+          .hint {
+              color: #909090;
+              font-size: 12px;
+          }
+        }
+
+        .el-button--text {
+          font-weight: 400;
+          font-size: 12px;
+          color: #007fff;
+        }
+
+        .avatar-wrapper {
+          position: relative;
+          margin-right: 10px;
+
+          .user-avatar {
+            cursor: pointer;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            border: 1px solid rgba(0, 0, 0, 0.1);
+          }
         }
       }
     }
